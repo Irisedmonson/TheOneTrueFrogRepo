912b5af519e523a26b24a2cb0bc740199b16c0d5
IrisEdmonson <irisedmonson@gmail.com>
2019-11-03 22:09:52 -0500

    New Level

---
+++ TheOneTrueFrog/Assets/Scripts/CompleteCameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteCameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    public bool bounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+
+    private Vector3 offset;           
+
+ 
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+  /*  void fixedUpdate()
+    {
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x), Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y), Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+    } */
+
+
+    void LateUpdate()
+    {
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x), Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y), Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+
+
+        transform.position = player.transform.position + offset;
+    }
+}
